91e7ca728c7bb91ad81d62085250da0cd83d2412
skokhanovskiy <s.kokhanovskiy@gmail.com>
2015-10-21 13:50:56 +0500

    Add New-ZabbixData help

--- ZabbixMonitoring/New-ZabbixData.ps1
+++ ZabbixMonitoring/New-ZabbixData.ps1
@@ -1,4 +1,32 @@
-﻿function New-ZabbixData
+﻿<#
+.SYNOPSIS 
+    Creates an item of zabbix data
+.DESCRIPTION
+    The New-ZabbixData function creates a hashtable with required information for sending to zabbix.
+.PARAMETER HostName
+    Specifies the monitored host name as registered in Zabbix frontend
+.PARAMETER Key
+    Specifies the key of zabbix item
+.PARAMETER HostName
+    Specifies the value of zabbix item
+.PARAMETER TimeStamp
+    Specifies the timestamp for a value
+.EXAMPLE
+    New-ZabbixData 'MYSQL1' 'mysql.queries' '347.4' | Send-ZabbixData 'zabbix'
+
+    New-ZabbixData function creates the zabbix data item with '347.4' as value for 'mysql.queries' key in 'MYSQL1' host. Send-ZabbixData function sends this value to zabbix server.
+.INPUTS
+    System.String
+.OUTPUTS
+    System.Object.Hashtable
+.LINKS
+    Send-ZabbixData
+.NOTES
+    The resulting hashtable will be converted to JSON as is before sending it to the zabbix server.
+
+    https://www.zabbix.org/wiki/Docs/protocols/zabbix_sender/2.0
+#>
+function New-ZabbixData
 {
     [CmdletBinding()]
     param
